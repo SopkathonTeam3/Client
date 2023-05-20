71c3d509f0aa36134a2e3a586682bc53b220554b
ljh0608 <m98m@catholic.ac.kr>
2023-05-21 04:58:33 +0900

    chore: svg컴포넌트 정리

---
+++ src/components/Icon/Icon.jsx
@@ -0,0 +1,4 @@
+import { ReactComponent as ModalClose } from 'assets/svgs/modal_close.svg';
+import { ReactComponent as ModalMessage } from 'assets/svgs/modal_message.svg';
+
+export { ModalClose, ModalMessage };
