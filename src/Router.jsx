5e3e5a3818422780fc118ff02a777cbd25d97c77
Yeonseo-Jo <jys073190@gmail.com>
2023-05-21 04:46:44 +0900

    feat : 유저페이지 기본 구조 테스트

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import React from 'react';
+import UserPage from './pages/UserPage';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<div>온보딩페이지를 넣어주세요</div>}></Route>
-        <Route path="/user" element={<div>유저이름 작성하는 페이지 </div>}></Route>
+        <Route path="/user" element={<UserPage />}></Route>
         <Route path="/custom" element={<div>커스텀 페이지를 넣어주세요</div>}></Route>
         <Route path="/loading" element={<div>로딩페이지를 넣어주세요</div>}></Route>
         <Route path="/main/:userid/:roomid" element={<div>메인페이지를 넣어주세요</div>}></Route>
--- src/pages/userPage.jsx
+++ src/pages/userPage.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const userPage = () => {
-  return <div>유저페이지입니당</div>;
+const UserPage = () => {
+  return (
+    <St.userPageWrapper>
+      <St.userInfoText>
+        <p>당신의 이름은 무엇인가요?</p>
+      </St.userInfoText>
+      <St.userNameInput></St.userNameInput>
+      <St.userSubmitBtn>버튼</St.userSubmitBtn>
+    </St.userPageWrapper>
+  );
 };
 
-export default userPage;
+export default UserPage;
 
 const St = {
   userPageWrapper: styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
     width: 100%;
+  `,
+  userInfoText: styled.div`
+    background-color: black;
+    & > p {
+      color: red;
+    }
+  `,
+  userNameInput: styled.input`
+    background-color: grey;
+  `,
+  userSubmitBtn: styled.button`
+    border: none;
   `,
 };
