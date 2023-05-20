5b8754d947638e28272361f83f89755d238fc361
HAE2NI <hae0_02ni@ewhain.net>
2023-05-21 03:18:48 +0900

    [style] theme.js

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,6 +1,27 @@
-const colors = {};
+const colors = {
+  blue: '#0089DB',
+  white: '#FFFFFF',
+  gray50: '#F2F4F6',
+  gray100: '#E2E4E7',
+  gray200: '#D2D6DB',
+  gray300: '#B1B8C0',
+  gray400: '#6D7582',
+  gray500: '#505866',
+  gray600: '#353C49',
+  gray700: '#1A1E27',
+  black: '#000000',
+};
 
-const fontSize = {};
+const fontSize = {
+  head0: 'font-size: 3.2rem; font-wight: 600',
+  subtitle1: 'font-size:1.8rem; font-wight: 600',
+  body1: 'font-size:1.8rem; font-wight: 400',
+  body2: 'font-size:1.6rem; font-wight: 500',
+  body3: 'font-size:1.4rem; font-wight: 400',
+  body4: 'font-size:1.2rem; font-wight: 400',
+  caption2: 'font-size:1.2rem; font-wight: 500',
+  caption1: 'font-size:1.0rem; font-wight: 700',
+};
 
 export const theme = {
   colors,
