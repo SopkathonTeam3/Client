a5c02dc464ffb20814edff55b94139c0b98edfea
Yeonseo-Jo <jys073190@gmail.com>
2023-05-21 04:13:05 +0900

    feat : 유저페이지 생성

---
+++ src/pages/userPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const userPage = () => {
+  return <div>유저페이지입니당</div>;
+};
+
+export default userPage;
+
+const St = {
+  userPageWrapper: styled.section`
+    width: 100%;
+  `,
+};
