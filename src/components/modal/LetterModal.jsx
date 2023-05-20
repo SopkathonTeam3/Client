8ab364a12acea5306318e1e5c6cfbd43e079f24b
HAE2NI <hae0_02ni@ewhain.net>
2023-05-21 05:23:46 +0900

    style: lettermodal

--- src/components/modal/LetterModal.jsx
+++ src/components/modal/LetterModal.jsx
@@ -8,22 +8,24 @@ const LetterModal = () => {
     <>
       <St.ModalWrapper>
         <St.ModalHeader>
-          <St.ModalHeaderTitle>
-            <St.ModalIconWrapper>
-              <St.ModalIcon src={close} alt="closemodal" />
-              <St.ModalIcon src={message} alt="message" />
-            </St.ModalIconWrapper>
-            윤여진님에게 <br />
-            편지를 남겨주세요!
-          </St.ModalHeaderTitle>
+          <St.ModalHeaderContent>
+            <St.ModalIconWrapper></St.ModalIconWrapper>
+            <St.ModalHeaderTitle>
+              윤여진님에게 <br />
+              편지를 남겨주세요!
+            </St.ModalHeaderTitle>
+          </St.ModalHeaderContent>
         </St.ModalHeader>
         <St.ModalMain>
           <St.ModalQuestion>Q.첫인상은 어땠나요?</St.ModalQuestion>
-          <St.ModalAnswerBox />
+          <p>
+            <St.ModalAnswerBox maxlength="50" placeholder="50자 내로 작성해주세요" />
+          </p>
 
           <St.ModalQuestion>Q.의외의 지점은 무엇이었나요?</St.ModalQuestion>
-          <St.ModalAnswerBox />
+          <St.ModalAnswerBox maxlength="50" placeholder="50자 내로 작성해주세요" />
         </St.ModalMain>
+        <St.ModalBtn>편지 띄우기 </St.ModalBtn>
       </St.ModalWrapper>
     </>
   );
@@ -45,6 +47,13 @@ const St = {
     background-color: ${({ theme }) => theme.color.white};
   `,
 
+  ModalHeaderContent: styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 31.9rem;
+    margin: 0 2.4rem;
+  `,
+
   ModalHeader: styled.header`
     display: flex;
     flex-direction: column;
@@ -54,6 +63,8 @@ const St = {
 
     width: 100%;
     height: 14.4rem;
+    border-top-right-radius: 1.2rem;
+    border-top-left-radius: 1.2rem;
 
     margin-bottom: 2.8rem;
   `,
@@ -61,10 +72,13 @@ const St = {
   ModalIconWrapper: styled.div`
     display: flex;
     justify-content: space-between;
+    align-items: stretch;
+    flex-direction: column;
+    width: 22.7rem;
+
+    height: 5rem;
   `,
-  ModalIcon: styled.img`
-    display: flex;
-  `,
+
   ModalHeaderTitle: styled.h1`
     color: ${({ theme }) => theme.color.gray700};
     ${({ theme }) => theme.text.subtitle1};
@@ -74,8 +88,10 @@ const St = {
     flex-direction: column;
   `,
 
-  ModalAnswerBox: styled.div`
+  ModalAnswerBox: styled.textarea`
     display: flex;
+    align-items: center;
+    justify-content: center;
     background-color: ${({ theme }) => theme.color.gray50};
     color: ${({ theme }) => theme.color.gray300};
 
@@ -84,6 +100,8 @@ const St = {
     height: 12rem;
 
     margin-bottom: 2.4rem;
+    padding: 2rem;
+    ${({ theme }) => theme.text.body2};
   `,
   ModalQuestion: styled.h2`
     color: ${({ theme }) => theme.color.gray700};
@@ -91,5 +109,19 @@ const St = {
     margin-bottom: 1.4rem;
   `,
 
-  ModalBtn: styled.button``,
+  ModalBtn: styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 31.9rem;
+    height: 6.9rem;
+
+    background-color: ${({ theme }) => theme.color.black};
+    color: ${({ theme }) => theme.color.white};
+    ${({ theme }) => theme.text.subtitle1};
+
+    border-bottom-left-radius: 1.2rem;
+    border-bottom-right-radius: 1.2rem;
+  `,
 };
