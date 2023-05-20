6c95994a0af45a2e4fdc02440adb7302eb3e72fd
ljh0608 <m98m@catholic.ac.kr>
2023-05-21 05:27:10 +0900

    chore: svg컴포넌트화

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,4 +1,8 @@
 import { ReactComponent as ModalClose } from 'assets/svgs/modal_close.svg';
 import { ReactComponent as ModalMessage } from 'assets/svgs/modal_message.svg';
+import { ReactComponent as BottleRight1 } from 'assets/svgs/bottle_right_1.svg';
+import { ReactComponent as BottleRight2 } from 'assets/svgs/bottle_right_2.svg';
+import { ReactComponent as BottleRight3 } from 'assets/svgs/bottle_right_4.svg';
+import { ReactComponent as BottleRight4 } from 'assets/svgs/bottle_right_8.svg';
 
-export { ModalClose, ModalMessage };
+export { ModalClose, ModalMessage, BottleRight1, BottleRight2, BottleRight3, BottleRight4 };
