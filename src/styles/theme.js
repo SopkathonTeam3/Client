ece00750b8eaa8ec50173b8529a9bff118c7f617
Yeonseo-Jo <jys073190@gmail.com>
2023-05-21 05:53:01 +0900

    style : 폰트 스타일 가이드 수정

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,4 +1,4 @@
-const colors = {
+const color = {
   blue: '#0089DB',
   white: '#FFFFFF',
   gray50: '#F2F4F6',
@@ -12,18 +12,20 @@ const colors = {
   black: '#000000',
 };
 
-const fontSize = {
+const text = {
   head0: 'font-size: 3.2rem; font-wight: 600; line-height: 3.8rem',
-  subtitle1: 'font-size:1.8rem; font-wight: 600; line-height: 2.0rem',
-  body1: 'font-size:1.8rem; font-wight: 400; line-height: 2.0rem',
-  body2: 'font-size:1.6rem; font-wight: 500; line-height: 2.0rem',
-  body3: 'font-size:1.4rem; font-wight: 400; line-height: 1.8rem',
-  body4: 'font-size:1.2rem; font-wight: 400; line-height: 1.6rem',
-  caption2: 'font-size:1.2rem; font-wight: 500; line-height: 1.6rem',
-  caption1: 'font-size:1.0rem; font-wight: 700; line-height: 1.2rem',
+  subtitle0: 'font-weight: 700; font-size: 2.8rem; line-height: 4rem;',
+  subtitle1: 'font-size: 1.8rem; font-wight: 600; line-height: 2.1rem',
+  subtitle2: 'font-size: 2rem; font-wight: 700; line-height: 2.8rem',
+  body1: 'font-size: 1.8rem; font-wight: 400; line-height: 2.7rem',
+  body2: 'font-size: 1.6rem; font-wight: 500; line-height: 1.9rem',
+  body3: 'font-size: 1.4rem; font-wight: 400; line-height: 1.7rem',
+  body4: 'font-size: 1.2rem; font-wight: 400; line-height: 1.4rem',
+  caption2: 'font-size: 1.2rem; font-wight: 500; line-height: 1.4rem',
+  caption1: 'font-size: 1.0rem; font-wight: 700; line-height: 1.2rem',
 };
 
 export const theme = {
-  colors,
-  fontSize,
+  color,
+  text,
 };
