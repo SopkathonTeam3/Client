81af9b20ae9e3be999d17b1b8d9ee414c3bb9c54
ljh0608 <m98m@catholic.ac.kr>
2023-05-21 02:55:34 +0900

    style: 경로 오류 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import Router from 'Router';
+import Router from './Router';
 import { ThemeProvider } from 'styled-components';
-import theme from './styles/theme';
-import GlobalStyle from 'styles/GlobalStyle';
+import { theme } from './styles/theme';
+import GlobalStyle from './styles/GlobalStyle';
 
 function App() {
   return (
