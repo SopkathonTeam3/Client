7638e9052b2ffec49bc29efae10ea36de5243287
ljh0608 <m98m@catholic.ac.kr>
2023-06-03 15:12:44 +0900

    useGetLetter re

--- src/recoil/useGetLetter.js
+++ src/recoil/useGetLetter.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 export const useGetLetter = async () => {
   try {
-    const response = await axios.get(`https://api.floatbottle.kro.kr/post/1/1`);
+    const { data } = await axios.get(`https://api.floatbottle.kro.kr/post/1/1`);
     console.log(response.data);
-    return response.data;
+    return data.data;
   } catch (e) {
     console.error(e);
   }
