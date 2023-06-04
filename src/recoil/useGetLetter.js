1a4cc916a050067893566b8e4cedc82ece097e48
seohee0112 <ghdtj7@naver.com>
2023-06-04 18:50:25 +0900

    docs : 필요없는 콘솔 삭제

--- src/recoil/useGetLetter.js
+++ src/recoil/useGetLetter.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 export const useGetLetter = async () => {
   try {
     const { data } = await axios.get(`https://api.floatbottle.kro.kr/post/1/1`);
-    console.log(response.data);
     return data.data;
   } catch (e) {
     console.error(e);
