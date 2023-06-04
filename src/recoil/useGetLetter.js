840969d30a8c2af25b1a8ae8d0eba92b7677f2a2
seohee0112 <ghdtj7@naver.com>
2023-06-04 18:57:43 +0900

    docs : 통신 함수 명 올바르게 수정

--- src/recoil/useGetLetter.js
+++ src/recoil/useGetLetter.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-
-export const useGetLetter = async () => {
+// 커스텀 훅에만 use를 사용하는것이 관행입니다!
+export const getLetter = async () => {
   try {
     const { data } = await axios.get(`https://api.floatbottle.kro.kr/post/1/1`);
     return data.data;
